fbf3675a1f81b64ab29f8df00f27ab698a95fe67
Jarkko Palokangas <jarkko.palokangas@solita.fi>
2023-12-04 20:31:19 +0200

    Add day 4 part 2 solution

--- day04.fsx
+++ day04.fsx
@@ -26,11 +26,47 @@ let calculatePoints (numbers: Set<int>) =
     | c when c = 0 -> 0
     | c -> Math.Pow(2 |> double, c - 1 |> double) |> int
 
+let commonNumbers (numbers: Set<int> array) =
+    Set.intersect numbers.[0] numbers.[1]
+
 let part1 =
     input
     |> Array.map toGame
-    |> Array.map (fun x -> Set.intersect x.[0] x.[1])
+    |> Array.map commonNumbers
     |> Array.map calculatePoints
     |> Array.sum
 
 part1 |> printfn "Part one: %A"
+
+let winningRange card numOfWins =
+    [(card + 1)..(card + numOfWins)]
+
+let emptyCardMap = input |> Array.mapi (fun i _ -> (i + 1, 1)) |> Map.ofArray
+
+let cardsWon (round: Set<int> array) =
+    round
+    |> Array.mapi (fun idx winNumbers ->
+        (idx + 1, winNumbers |> Set.count)
+    )
+    |> Array.fold (fun (cardMap: Map<int,int>) curWinningNumbers ->
+        let (first, last) = curWinningNumbers
+        let cardsWon = winningRange first last
+        
+        cardsWon
+        |> List.fold (fun numberMap number ->
+            let old = numberMap.[number]
+            let multiplier = numberMap.[curWinningNumbers |> fst]
+            numberMap |> Map.change number (fun _ -> Some (old + multiplier))
+        ) cardMap
+    ) emptyCardMap
+
+let part2 =
+    input
+    |> Array.map toGame
+    |> Array.map commonNumbers
+    |> cardsWon
+    |> Map.toArray
+    |> Array.map snd
+    |> Array.sum
+
+part2 |> printfn "Part two: %A"
