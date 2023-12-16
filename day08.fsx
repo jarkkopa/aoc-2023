9fcea6f1248dd7ae70d4bcfcc59850c3c697f7ad
Jarkko Palokangas <jarkko.palokangas@solita.fi>
2023-12-16 18:04:20 +0200

    Add day 8 part 2 solution

--- day08.fsx
+++ day08.fsx
@@ -7,8 +7,6 @@ type Instruction = L | R
 let nodeRx = Regex(@"(\w{3})\s=\s\((\w{3}),\s(\w{3})\)")
 let input = "inputs/day08.txt" |> System.IO.File.ReadAllLines
 
-"BBB = (AAA, ZZZ)" |> nodeRx.Match |> (fun x -> (x.Groups.[1].Value, x.Groups.[2].Value, x.Groups.[3].Value))
-
 let toInstructions line =
     line
     |> Seq.toArray
@@ -30,16 +28,46 @@ let nodeMap =
     |> Array.removeManyAt 0 2
     |> toMap
 
-let rec nextNode (instr: Instruction array)  steps current (routeMap: Map<string, (string * string)>) =
+let rec nextNode goal (instr: Instruction array)  steps current (routeMap: Map<string, (string * string)>) =
     let nextRoute = routeMap[current]
     let direction = instr.[(steps - 1) % instr.Length]
     let next = match direction with | L -> fst nextRoute | R -> snd nextRoute
-    match next with
-    | "ZZZ" -> steps
-    | _ -> nextNode instr (steps + 1) next routeMap
+
+    match goal next with
+    | true -> steps
+    | false -> nextNode goal instr (steps + 1) next routeMap
 
 let part1 =
     nodeMap
-    |> nextNode instructions 1 "AAA"
+    |> nextNode (fun n -> n = "ZZZ") instructions 1 "AAA"
 
 part1 |> printfn "Part one: %A"
+
+let startNodes = nodeMap |> Map.toArray |> Array.filter (fun (x,_) -> x.EndsWith "A") |> Array.map fst;; 
+
+let stepsToGoal =
+    startNodes
+    |> Array.map (fun n ->
+        nodeMap
+        |> nextNode (fun n -> n.EndsWith "Z") instructions 1 n
+        |> int64
+    )
+
+let rec gcd (a: int64) (b: int64) =
+    match b with
+    | 0L -> abs a
+    | _ -> gcd b (a % b)
+
+let lcmPair (a: int64) (b: int64) = a * b / (gcd a b)
+
+let rec lcm = function
+    | [a;b] -> lcmPair a b
+    | head::tail -> lcmPair (head) (lcm (tail))
+    | [] -> 1
+
+let part2 =
+    stepsToGoal
+    |> List.ofArray
+    |> lcm
+
+part2 |> printfn "Part two: %A"
